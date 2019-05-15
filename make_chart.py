7e8649181c3488ddec55f9f12550c47e776042f5
RandyRomero <a.ya.miheev@gmail.com>
2019-05-15 18:59:03 +0300

    Fix an oversight, add prints to denote what is going on

--- make_chart.py
+++ make_chart.py
@@ -4,19 +4,21 @@ import plotly.graph_objs as go
 from plotly.offline import init_notebook_mode
 import plotly.io as pio
 
+from config import path_to_orca
 from prepare_data import prepare_data
 init_notebook_mode(connected=True)
 
-if not os.path.exists('/images'):
+if not os.path.exists('images'):
     os.mkdir('images')
-
 
 # todo make it accept command line arguments
 
-pio.orca.config.executable = '../orca/orca-1.2.1-x86_64.AppImage'
+pio.orca.config.executable = path_to_orca
 
+print('Start preparing data for a plot...')
 y_axis_names, x_axis_values, x_axis_values2 = prepare_data()
 
+print('Start making the plot...')
 length = len(y_axis_names)
 
 trace1 = go.Bar(
@@ -58,4 +60,6 @@ layout = go.Layout(title='Geekbench 4',
 fig = go.Figure(data=data, layout=layout)
 fig['layout']['margin'] = {'l': 350}
 
+print('Start rendering the plot...')
 pio.write_image(fig, 'images/GeekBench.png', width=1366, height=(1366 * 2))
+print('Plot was saved as images/GeekBench.png')
